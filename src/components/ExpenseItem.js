cfea271c65200d6cba2439712dec31ab43825353
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-19 18:28:55 +0000

    added nested DOM elements to root element

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        // root element has to be 1 element, nesting all other elements
+        <div>
+            <div>19/03/2022</div>
+            <div>
+                <h2>Car Loan</h2>
+                <div>€ 220.00</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
