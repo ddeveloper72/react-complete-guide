00de8f15aa8976522fc523c31422420de7eba8c6
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 09:40:50 +0000

    setup framework for the date elements

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,18 @@
  
-function ExpenseDate() {}
+function ExpenseDate(props) {
+
+    // define specific date elements as constants
+    const month = props.date.toLocaleString('ie-EU', { month: 'long' });
+    const day = props.date.toLocaleString('ie-EU', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    return (
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+    )
+}
 
 export default ExpenseDate;
