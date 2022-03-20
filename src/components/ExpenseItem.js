7e88cb12eeadda35e8a0a67b9d0f8bd5b5d34cd5
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 09:41:12 +0000

    import the date elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-    // define specific date elements as constants
-    const month =  props.date.toLocaleString('ie-EU', {month: 'long'});
-    const day = props.date.toLocaleString('ie-EU', {day: '2-digit'});
-    const year = props.date.getFullYear();
-
     return (
         // root element has to be 1 element, nesting all other elements
-        // create individual elements for the date values
+        // use an import element tag for the date element
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
