0f506e6bf331c0160c0183f7ba7b7a6f828138fd
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 09:51:48 +0000

    add date props tag to importe data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,9 +5,10 @@ function ExpenseItem(props) {
 
     return (
         // root element has to be 1 element, nesting all other elements
-        // use an import element tag for the date element
+        // use an import element tag for ExpenseDate then inherit data 
+        // from the date key value from expense object in App.js
         <div className='expense-item'>
-            <ExpenseDate/>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
