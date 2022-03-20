dad1f51c6bb108d2e81fd63ef0142aaac0dfa4fc
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 09:24:26 +0000

    refactored date constans better suited for calander layout

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,17 +4,17 @@ function ExpenseItem(props) {
 
     // define specific date elements as constants
     const month =  props.date.toLocaleString('ie-EU', {month: 'long'});
-    const day = props.date.toLocaleString('ie-EU', {weekday: 'long'});
-    const year = props.date.toLocaleString('ie-EU', { year: 'numeric'});
+    const day = props.date.toLocaleString('ie-EU', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         // root element has to be 1 element, nesting all other elements
         // create individual elements for the date values
         <div className='expense-item'>
             <div>
-                <div>{day}</div>
                 <div>{month}</div>
                 <div>{year}</div>
+                <div>{day}</div>
             </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
