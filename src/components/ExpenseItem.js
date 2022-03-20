1c278bb80d1e0136b435f1cf4df8683afb951047
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 08:13:30 +0000

    converte hard coded data to re-usable constants

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    // add constants
+    const expenseDate = new Date(2022, 3, 20);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 220.00;
+
     return (
         // root element has to be 1 element, nesting all other elements
+        // remember to convert date object to a string
         <div className='expense-item'>
-            <div>19/03/2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Loan</h2>
-                <div className='expense-item__price'>€ 220.00</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>€ {expenseAmount}</div>
             </div>
         </div>
     );
