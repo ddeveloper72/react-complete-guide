e65d6dc98b2a7f2f844b509cf785d03f08961ab3
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-20 09:08:07 +0000

    used localestrings to split up date variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,20 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+    // define specific date elements as constants
+    const month =  props.date.toLocaleString('ie-EU', {month: 'long'});
+    const day = props.date.toLocaleString('ie-EU', {weekday: 'long'});
+    const year = props.date.toLocaleString('ie-EU', { year: 'numeric'});
+
     return (
         // root element has to be 1 element, nesting all other elements
-        // remember to convert date object to a string
+        // create individual elements for the date values
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
