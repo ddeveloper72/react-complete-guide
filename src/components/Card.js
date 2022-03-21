ea415041660c2ab8706d232b79ac9a893d6d9edd
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-21 21:14:05 +0000

    use props to prompt inheritence between the components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,10 @@
- function Card() {
+ import './Card.css';
+ 
+ function Card(props) {
+     // remember to include a space when joining two strings so they read as separate strings
+     const classes = 'card ' + props.className;
 
-    return <div></div>
+    return <div className={classes}>{props.children}</div>
  }
 
- export default Card
+ export default Card;
