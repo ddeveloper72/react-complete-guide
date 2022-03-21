471dd7409dab2b99a8c46d94c7c4dfd0cf654dba
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-21 21:14:58 +0000

    import & deploy custom Card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
 
@@ -7,13 +8,13 @@ function ExpenseItem(props) {
         // root element has to be 1 element, nesting all other elements
         // use an import element tag for ExpenseDate then inherit data 
         // from the date key value from expense object in App.js
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
