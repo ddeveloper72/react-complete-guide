5c3a5152a03bc76118b71ac879d7aeed5f8a11a4
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 11:34:22 +0000

    refactored function to => syntax

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
     // remember to include a space when joining two strings so they read as separate strings
     const classes = 'card ' + props.className;
 
