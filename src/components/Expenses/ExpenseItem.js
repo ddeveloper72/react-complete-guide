ea2bb93153f8a870b972630c5573f77c0cf4f59f
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 13:24:37 +0000

    demonstrate changing props value

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,9 +5,11 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+    let title = props.title;
     // the click handler evaluated the click only once the DOM has loaded.
     const clickHandler = () => {
-        console.log('Clicked!!');
+        title = 'Clicked!!'
+        console.log(title);
     };
 
     return (
@@ -17,7 +19,7 @@ const ExpenseItem = (props) => {
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
