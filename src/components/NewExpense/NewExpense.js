cb3cfd41c722170c731a687bbb0aee1f3b79e36f
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 20:00:28 +0000

    import ExpenseForm as its own component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './NewExpense.css';
+import './ExpenseForm'
 
 const NewExpense = () => {
-// build a form for user inputs
 
-return (
-    <div>
-        <form></form>
-    </div>
-)
+    return (
+        <div className='new-expense'></div>
+    )
 };
 
 export default NewExpense;
