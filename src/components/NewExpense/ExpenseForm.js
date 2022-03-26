9307bfe5932b4f5f7bf7011ee18528aaac55d9cf
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 20:04:50 +0000

    added 1st div for title

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,10 @@ import './ExpenseForm.css';
 const ExpenseForm = () => {
     <form>
         <div className='new-expense__controls'>
-
+            <div className='new-expense___control'>
+                <label>Title</label>
+                <input type='text' />
+            </div>
         </div>
     </form>
 }
