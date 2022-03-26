ed27fc67f9293318cafe715fbe0cd5179591f571
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 19:47:42 +0000

    added css import

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './NewExpense.css';
 
 const NewExpense = () => {
+// build a form for user inputs
 
+return (
+    <div>
+        <form></form>
+    </div>
+)
 };
 
 export default NewExpense;
