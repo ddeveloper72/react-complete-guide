e33f0905780e273b50889c3e9192541f3123a8a0
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 22:51:54 +0000

    determin the value from the event object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,12 +4,17 @@ import './ExpenseForm.css';
 // build a form for user inputs
 
 const ExpenseForm = () => {
+    const TitleChangeHandler = (event) => {
+        // use the vanilla JS event object to get the event, then map the event to get the value from the console
+        console.log(event);
+    };
+    
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={TitleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
