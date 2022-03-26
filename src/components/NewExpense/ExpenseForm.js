8c92a292196c39f604918ccfbdce5c77b3945b8c
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-26 20:00:57 +0000

    added boilerplate for ExpenseForm component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+// build a form for user inputs
+
+const ExpenseForm = () => {
+    <form>
+        <div className='new-expense__controls'>
+
+        </div>
+    </form>
+}
+
+
+export default ExpenseForm;
