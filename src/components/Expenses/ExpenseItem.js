bd2cbb0b91a60d56961b6523eadec6af33abfcbd
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-30 22:21:57 +0100

    remove chage title button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-    // add react hook, with an array of current value and function for changed value
-    const [title, setTitle] = useState(props.title);
-
-    // let title = props.title;
-    // the click handler evaluated the click only once the DOM has loaded.
-    const clickHandler = () => {
-        // call SetTitle to reevaluate ExpenseItem, using the newly defined value 
-        // for the initial state value
-        setTitle('Updated');
-    };
 
     return (
         // root element has to be 1 element, nesting all other elements
@@ -23,10 +13,9 @@ const ExpenseItem = (props) => {
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>€ {props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
