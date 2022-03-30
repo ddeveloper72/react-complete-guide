ba0e05c842e9252e5d7f07747cc783871858d1db
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-30 23:04:56 +0100

    use map to iterate through the array of expense items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,15 +16,20 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  // console.log(props);
+  // use map to iterate through the array of expense items
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter onChangeFilter={filterChangeHandler} selectedYear={filteredYear} />
-        <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}></ExpenseItem>
-        <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}></ExpenseItem>
-        <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}></ExpenseItem>
-        <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}></ExpenseItem>
+        {
+          props.item.map((expense) => (
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            ))
+        }
       </Card>
     </div>
   )
