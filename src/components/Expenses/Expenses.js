7aa0e5aae6def05f4a8e38662e30c36912838dd2
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-30 21:35:14 +0100

    create a new props for the filtered year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
@@ -19,13 +19,13 @@ const Expenses = (props) => {
   // console.log(props);
   return (
     <div>
-    <Card className="expenses">
-      <ExpenseFilter onChangeFilter={filterChangeHandler}/>
-      <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}></ExpenseItem>
-      <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}></ExpenseItem>
-      <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}></ExpenseItem>
-      <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}></ExpenseItem>
-    </Card>
+      <Card className="expenses">
+        <ExpenseFilter onChangeFilter={filterChangeHandler} selectedYear={filteredYear} />
+        <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}></ExpenseItem>
+        <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}></ExpenseItem>
+        <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}></ExpenseItem>
+        <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}></ExpenseItem>
+      </Card>
     </div>
   )
 }
