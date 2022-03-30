1bd5085e3df263d2cc7b83a74cf3d12cf696f6d3
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-30 21:01:37 +0100

    add imports for ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
 
+  // console.log(props);
   return (
+    <div>
     <Card className="expenses">
+        <ExpenseFilter item={ExpenseItem}/>
       <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}></ExpenseItem>
       <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}></ExpenseItem>
       <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}></ExpenseItem>
       <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}></ExpenseItem>
     </Card>
+    </div>
   )
 }
 
