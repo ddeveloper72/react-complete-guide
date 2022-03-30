7937fcfb8e90c9ea036385283a90dbdd2e51ea30
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-30 23:46:22 +0100

    construct a new array called expenses based on status

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 
-const App = () => {
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2022, 2, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 220.00,
-      date: new Date(2022, 3, 20),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2022, 1, 12),
-    },
-  ];
+const SAMPLE_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2022, 2, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 220.00,
+    date: new Date(2022, 3, 20),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2022, 1, 12),
+  },
+];
+
+
+const App = () => {
+  // deconstructed the the array into an object and a function
+  const [expenses, setExpenses] = useState(SAMPLE_EXPENSES);
 
   // function for collecting expense data from  ExpenseForm
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
-
-  // Standard Code
-
-  // return React.createElement(
-  //   'div', {},
-  //   React.createElement('h2', {}, 'Let\'s get started!'),
-  //   React.createElement(Expenses, { item: expenses })
-  // );
-
+  // and construct a new array called expenses based on status
+  const addExpenseHandler = (expense) => {
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
+  };
 
   // JSX Code
 
