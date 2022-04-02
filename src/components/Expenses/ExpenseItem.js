916158c40e64da425cb3ee53e7c0ddd60556cc50
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-02 12:47:26 +0100

    placed the card element into a li item since content is an ul

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,13 +10,15 @@ const ExpenseItem = (props) => {
         // root element has to be 1 element, nesting all other elements
         // use an import element tag for ExpenseDate then inherit data 
         // from the date key value from expense object in App.js
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>€ {props.amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{props.title}</h2>
+                    <div className='expense-item__price'>€ {props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
