edef77ff446bf49d37b374ef17fe3ee4eb276417
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-02 12:44:30 +0100

    move all logic for ExpenseList to its own component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 import Card from '../UI/Card';
 
@@ -21,28 +21,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // use map to iterate through the array of filteredExpenses items
-  let expensesContent = <p>No expenses found.</p>;
-
-  // use conditional logic to iterate through the array of filteredExpenses items
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
-
   // refer to a constant to get logic
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter onChangeFilter={filterChangeHandler} selectedYear={filteredYear} />
-        {expensesContent}
+        <ExpensesList item={filteredExpenses}/>
       </Card>
     </div>
   )
