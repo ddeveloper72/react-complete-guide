68cf1e7efc568d2e999b34a96283c0924a1c1701
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-02 12:43:48 +0100

    setup logic for expense list content

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,5 +1,32 @@
- import React from 'react';
+import React from 'react';
 
-const ExpensesList = props {
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = props => {
+
+    // declare a constant for expenses content
+    let expensesContent = '';
+
+    // use logic to iterate through the array of filteredExpenses items
+    if (props.item.length > 0) {
+        expensesContent = props.item.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+    else {
+        expensesContent = <h2 className='expenses-list__fallback'>Found no Expenses.</h2>;
+    }
+
+    return <ul className='expenses-list'>
+        {expensesContent}
+    </ul>
 
 };
+
+export default ExpensesList;
