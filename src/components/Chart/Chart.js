770579703b8ce003d876a59ed519e5093f94111a
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-03 11:00:01 +0100

    create 2 constants for calculating the data values

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,6 +6,13 @@ import './Chart.css';
 // chart data is inherited through props
 const Chart = (props) => {
 
+    // create a variable for returning and [] of data values
+    const dataValue = props.data
+        .map(data => data.value);
+
+    // create a variable for calculating the maxValue
+    const totatlMaximum = Math.max();
+
     // render the chart in the DOM
     return <div className='chart'>
         {props.data
