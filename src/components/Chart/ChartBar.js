253295b16d20c7dd1cee91ad8391e5a2a6831889
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-03 10:17:17 +0100

    add props for label data

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const ChartBar = () => { };
+import './ChartBar.css'
+
+const ChartBar = (props) => {
+    return <div className='chat-bar'>
+        <div className='chart-bar__inner'>
+            <div className='chart-bar__fill'></div>
+        </div>
+        <div className='chart-bar__label'>{props.label}</div>
+    </div>
+};
 
 export default ChartBar;
