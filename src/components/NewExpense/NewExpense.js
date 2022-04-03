deb6eff95b798a403dd5dd550476c1b3dd9f2bb6
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-03 08:27:17 +0100

    useState to setup DOM elements for button or form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import '../NewExpense/NewExpense.css';
 
 const NewExpense = (props) => {
+
+    // create a variable name for DOM object add expense button || expenseForm
+    let SelectNewExpense = '';
+
+    // get initial state of expense form which is not shown- false
+    const [addingNewExpense, setAddNewExpense] = useState(false);
+
     const SaveNewExpenseData = (enteredExpenseData) => {
         //  use the spread operation to add the new data to the existing array
         //  added id to the data using math.random()
@@ -10,14 +17,31 @@ const NewExpense = (props) => {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        // console.log(expenseData);
+
         props.onAddExpense(expenseData);
     };
 
+    // call and addNewExpenseHandler method to change the setAddNewExpense state to true.
+    const addNewExpenseHandler = () => {
+        setAddNewExpense(true);
+    };
+
+    if (!addingNewExpense) {
+        SelectNewExpense =
+        // when setAddNewExpense is false, show the addingNewExpense button
+            <button onClick={addNewExpenseHandler}>Create New Expense</button>;
+
+    } else {
+        // when setAddNewExpense is true, show the setAddNewExpense form
+        SelectNewExpense =
+            <ExpenseForm onSaveNewExpenseData={SaveNewExpenseData} />;
+
+    }
+
     return (
-        // add a prop, onSaveNewExpenseData to pull in function as a value
+        // inject logic for useState here 👍
         <div className='new-expense'>
-            <ExpenseForm onSaveNewExpenseData={SaveNewExpenseData}/>
+            {SelectNewExpense}
         </div>
     );
 };
