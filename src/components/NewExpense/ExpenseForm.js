46d65827709800dd6f592300246a8cb1cb23d51b
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-03 09:18:27 +0100

    added click listener for cancel subutton

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -62,6 +62,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.cancelSubmit}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
