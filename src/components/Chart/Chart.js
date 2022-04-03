81b53606b7def010007cb37620b2923dbe83f466
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-03 09:53:38 +0100

    add imports and return DOM element place holder

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Chart = () => { };
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+// chart data is inherited through props
+const Chart = (props) => {
+
+    // render the chart in the DOM
+    return <div className='chart'></div>
+};
 
 export default Chart;
